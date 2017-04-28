ba09708d072ab9407971524a613ac52709d4d743
Saurav Biswas <saru.sreyo@gmail.com>
2017-04-28 11:06:45 +0530

    States Exercise

--- src/app/index.js
+++ src/app/index.js
@@ -7,18 +7,12 @@ var TodoComponent = React.createClass({
     render:function(){
         return(
             <div>
-            <h1>Name: {this.props.cheese.name}</h1>
-            <h3>Smell Factor: {this.props.cheese.smellFactor}</h3>
-            <h3>{this.props.cheese.price}</h3>
+            
             </div>
         );
     }
 });
 
-var myCheese = {
-    name:"Camembert",
-    smellFactor:"Extreme Pong",
-    price:'350'
-};
+
 // Put component to HTML page
-ReactDom.render(<TodoComponent msg ="I love cheese" cheese={myCheese}/>,document.getElementById('todo-wrapper'));
+ReactDom.render(<TodoComponent/>,document.getElementById('todo-wrapper'));
